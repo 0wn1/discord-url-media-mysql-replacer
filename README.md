9b57eca67353be29b3431b89ec98949efd81c0ee
0wn1 <thebigboy@outlook.com.br>
2024-03-17 22:18:45 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+## Discord URL Media MySQL Replacer
+
+This application is designed to automate the process of replacing invalid URLs media stored in a MySQL database with valid media URLs fetched from Discord messages.
+
+- **Streamable**: [View](https://streamable.com/ehwgq5)
+________________________________________________________________
+
+### Usage:
+1. **Prerequisites:**
+   - Install [Node.js](https://nodejs.org/en/download/) on your operating system.
+   - Open the `index.js` file in a text editor ([Visual Studio Code](https://code.visualstudio.com/Download) or [Notepad++](https://notepad-plus-plus.org/downloads/)).
+   - Adjust the `config`, `dbConfig`, and `tables` variables according to your Discord bot token, MySQL database configuration, and table/column names.
+
+2. **Backup your database:**
+   - It's optional but highly recommended to create a backup of your database before running the application.
+
+3. **Installation:**
+   - Run `install.bat` and wait for the libraries to be transferred.
+
+4. **Execution:**
+   - Run `start.bat` and wait for the process to complete.
+   - Upon completion, press `CTRL+C` in your terminal to stop the execution and close the window.
+
+________________________________________________________________
+
+### Notes:
+- Sometimes, the application may fail to find and replace the URL in the database. If this occurs, repeat the process.
+- It's advisable to run the application before starting your server.
+- This application has not been tested extensively on databases with large datasets. Adjustments may be required for optimal performance.
---
+++ index.js
@@ -0,0 +1,84 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const mysql = require('mysql2');
+
+const Config = {
+	token: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // Discord App Token: https://discord.com/developers/applications
+	channelId: 'xxxxxxxxxxxx', // Text Channel AppdId
+	DeleteMessages: true // Auto deletes sent messages after replacing media URLs
+};
+
+Config.Database = {
+	host: 'localhost', // Database host adress
+	user: 'root', // Database username
+	password: '', // Database password
+	database: 'creative' // Database name
+};
+
+Config.Tables = [{
+		name: 'smartphone_gallery', // Database table name
+		column: 'url' // Database column name
+	},
+	/* {
+		name: 'tablename2',
+		column: 'columname'
+	} */
+];
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const db = mysql.createConnection(Config.Database);
+
+async function FetchAndUpdateImages() {
+	for (const table of Config.Tables) {
+		let count = 1;
+		const query = `SELECT ${table.column} FROM ${table.name}`;
+
+		await new Promise((resolve, reject) => {
+			db.query(query, async (QueryError, results) => {
+				if (QueryError) return reject(QueryError);
+
+				for (const row of results) {
+					const OldURL = row[table.column];
+					try {
+						const ValidURL = await GetProxyURL(OldURL);
+						const Update = `UPDATE ${table.name} SET ${table.column} = ? WHERE ${table.column} = ?`;
+
+						db.query(Update, [ValidURL, OldURL], (UpdateError) => {
+							if (UpdateError) throw UpdateError;
+							count++;
+						});
+					} catch (err) {
+						console.error(err.message);
+					}
+				}
+				console.log(`${count} URLs replaced from ${table.name}`);
+				resolve();
+			});
+		});
+	}
+}
+
+async function GetProxyURL(url) {
+
+	const channel = client.channels.cache.get(Config.channelId);
+	const Message = await channel.send(url);
+
+	const ValidURL = Message.embeds[0]?.data?.thumbnail?.proxy_url
+
+	if (Config.DeleteMessages) {
+		await Message.delete();
+	}
+
+	if (ValidURL) {
+		return ValidURL;
+	} else {
+		throw new Error('URL not found ');
+	}
+}
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}`);
+	FetchAndUpdateImages();
+});
+
+client.login(Config.token);
